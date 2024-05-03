85597bf02074ae65c762ce8800237c4b30253460
Walther <wszin06@gmail.com>
2024-05-03 19:42:40 -0300

    entidade usuario

--- backend/entity/Usuario.php
+++ backend/entity/Usuario.php
@@ -1,5 +1,4 @@
 <?php
-
 class Usuario {
     // Propriedades
     private $id;
@@ -10,26 +9,51 @@ class Usuario {
     private $ativo;
     private $dataCriacao;
     private $dataAtualizacao;
+
+    // Método Construtor
+    public function __construct($nomeUsuario, $senha, $email, $grupoUsuarioID, $ativo = 1, $dataCriacao = null, $dataAtualizacao = null) {
+        $this->nomeUsuario = $nomeUsuario;
+        $this->senha = $senha;
+        $this->email = $email;
+        $this->grupoUsuarioID = $grupoUsuarioID;
+        $this->ativo = $ativo;
+        $this->dataCriacao = $dataCriacao;
+        $this->dataAtualizacao = $dataAtualizacao;
+    }
+
+    // Getters
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNomeUsuario() {
+        return $this->nomeUsuario;
+    }
+
+    public function getSenha() {
+        return $this->senha;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getGrupoUsuarioId() {
+        return $this->grupoUsuarioID;
+    }
+
+    public function getAtivo() {
+        return $this->ativo;
+    }
+
+    public function getDataCriacao() {
+        return $this->dataCriacao;
+    }
+
+    public function getDataAtualizacao() {
+        return $this->dataAtualizacao;
+    }
     
-    // Método construtor
-
-    // getters
-
-    // setters
+    // Setters
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ?>
