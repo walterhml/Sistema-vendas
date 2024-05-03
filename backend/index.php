ad215db894844f0c0fbeeddbf6c00dfda9bf4f00
Walther <wszin06@gmail.com>
2024-05-02 22:11:45 -0300

    project

---
+++ backend/index.php
@@ -0,0 +1,10 @@
+<?php
+require_once("template/header.php");
+?>
+
+<h1>ola sistemas venda body</h1>
+</body>
+
+<?php
+require_once("template/footer.php");
+?>
---
+++ backend/template/footer.php
@@ -0,0 +1,7 @@
+<footer>
+
+        radape da pagina
+
+</footer>
+
+</html>
