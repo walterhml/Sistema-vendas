408eefc2663480b78bdcaad869a2df9ed82909db
Walther <wszin06@gmail.com>
2024-05-09 19:14:12 -0300

    ola

--- backend/authservice.php
+++ backend/authservice.php
@@ -9,17 +9,17 @@ if($type === "register") {
     // Lógica de registro do usuário
     $new_nome = filter_input(INPUT_POST, "new_nome");
     $new_email = filter_input(INPUT_POST, "new_email", FILTER_SANITIZE_EMAIL);
-    $new_password = filter_input(INPUT_POST, "new_password");    
+    $new_password = filter_input(INPUT_POST, "new_password");
     $confirm_password = filter_input(INPUT_POST, "confirm_password");   
-       
+    
     if($new_email && $new_nome && $new_password) {
         if($new_password === $confirm_password) {
             $hashed_password = password_hash($new_password, PASSWORD_DEFAULT);
 
             $usuario = new Usuario(null, $new_nome, $hashed_password, $new_email, null, null, null, null);    
             $usuarioDAO = new UsuarioDAO();
-            $usuarioDAO->create($usuario);
-            
+            $success = $usuarioDAO->create($usuario);
+
             if($success) {
                 header("Location: index.php");
                 exit();
@@ -32,7 +32,6 @@ if($type === "register") {
     } else {
         // TODO: exibir mensagem de formulário inválido
     }
-
 } elseif ($type === "login") {
     // TODO: verificar se o usuário tem cadastro
     // dar ao usuário um token de sessão para navegar no site
